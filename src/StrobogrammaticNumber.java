70637810bd14b3c5d3a266cfeb61f800bd8c7ad8
Sikai Zhao <sikaizhao1997@gmail.com>
2020-05-26 00:20:34 -0400

    246

---
+++ src/StrobogrammaticNumber.java
@@ -0,0 +1,23 @@
+public class StrobogrammaticNumber {
+    public boolean isStrobogrammatic(String num) {
+        char[] strs = num.toCharArray();
+        int l = num.length();
+
+        for (int i = 0; i <= l / 2; i++) {
+            if (!(strs[i] == '1' && strs[l - 1 - i] == '1'
+                    || strs[i] == '0' && strs[l - 1 - i] == '0'
+                    || strs[i] == '8' && strs[l - 1 - i] == '8'
+                    || strs[i] == '6' && strs[l - 1 - i] == '9'
+                    || strs[i] == '9' && strs[l - 1 - i] == '6')) {
+                return false;
+            }
+        }
+        return true;//strs[l / 2] == '1' && strs[l - 1 - l / 2] == '1' || strs[l / 2] == '8' && strs[l - 1 - l / 2] == '8';
+    }
+
+    public static void main(String[] args) {
+        String s = "69";
+        StrobogrammaticNumber strobogrammaticNumber = new StrobogrammaticNumber();
+        System.out.println(strobogrammaticNumber.isStrobogrammatic(s));
+    }
+}
